add7596e14e2b75a17300f5dd3b9c5c09607d111
cyw <chenyaowen1993@gmail.com>
2019-11-14 11:34:11 +0800

    :wrench: update some config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,12 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
