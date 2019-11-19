11163e162b1df5da897c6ef42146c78f2a976ac8
cyw <cyw19931210@163.com>
2019-11-19 10:53:49 +0800

    :package: ckeditor

---
+++ src/components/Editor/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ckeditor">
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+export default {
+  name: 'Editor',
+  components: {
+    // Use the <ckeditor> component in this view.
+    ckeditor: CKEditor.component,
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: '<p>Content of the editor.</p>',
+      editorConfig: {
+        // The configuration of the editor.
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.ckeditor {
+}
+</style>
